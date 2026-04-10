8a8f5510c16ca92af3e09e5da3a62a8d89f02988
Bharath ranganathan <bharathranganathan22@gmail.com>
2026-04-11 02:01:01 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    bucket         = "bharath-eks-2026"
+    region         = "us-west-2"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
+    dynamodb_table = "Eks-lock-table"
     encrypt        = true
   }
   required_version = ">=0.13.0"
