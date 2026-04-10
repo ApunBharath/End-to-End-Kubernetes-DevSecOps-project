d87867165761c47353e9ae7278e1c27b4ac0a984
Bharath <bharathranganathan22@gmail.com>
2026-04-10 21:03:04 +0000

    Clean repo without terraform cache

---
+++ EKS-TF/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "bharath-eks-2026"
+    region         = "us-west-2"
+    key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
+    dynamodb_table = "Eks-lock-table"
+    encrypt        = true
+  }
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
